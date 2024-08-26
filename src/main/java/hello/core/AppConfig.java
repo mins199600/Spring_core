d2ea70f52922ebba2438d2fa2d7f768aa9547304
sumin <mins19960091@gmail.com>
2024-08-26 20:56:52 +0900

    appConfig flxedPolicy 리펙토링 수정, RatePolicy 기능 수정 예정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixdiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,12 +11,19 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
     //구현 객체를 생성하고 생성자를 통해서 연결해주는곳
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    public DiscountPolicy discountPolicy() {
+        return new FixdiscountPolicy();
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixdiscountPolicy());
-    }
 
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,11 +1,8 @@
 package hello.core.order;
-
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixdiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
+
 
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
